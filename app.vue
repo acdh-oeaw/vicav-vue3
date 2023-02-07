f62ddf58e8683b7d430b7b39ae488b2931073af9
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2023-02-07 18:19:58 +0100

    gitignore amended

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { $api } = useNuxtApp();
+
+let projectInfo = await $api.project.getProject({mode: "no-cors", headers: { ACCEPT: "application/json" }});
+
+</script>
+<template>
+  <div>
+    This is becoming VICAV on Nuxt.
+    <p>{{ JSON.stringify(projectInfo.data) }}</p>
+  </div>
+</template>
