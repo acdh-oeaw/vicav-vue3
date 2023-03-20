8967ad351611f7333e437d8f6a0d36ba088309f6
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2023-03-20 17:07:26 +0100

    minimal winbox test

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
-
+import { VicavWinBox } from "~/components/VicavWinBox.client";
+const options = {
+  x: 500,
+  y: 200
+}
 </script>
 <template>
+  <client-only>
+    <VicavWinBox :options="options"></VicavWinBox>
+  </client-only>
 this is a test page
 </template>
