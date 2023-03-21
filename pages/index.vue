0e6177a1d0888aec223593c5661eb649bfb0dd3e
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2023-03-21 13:33:03 +0100

    some window content

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { VicavWinBox } from "~/components/VicavWinBox.client";
+import { LMap, LMarker, LTileLayer, LFeatureGroup } from "@vue-leaflet/vue-leaflet";
+
 const options = {
   x: 500,
   y: 200
@@ -7,7 +9,9 @@ const options = {
 </script>
 <template>
   <client-only>
-    <VicavWinBox :options="options"></VicavWinBox>
+    <VicavWinBox :options="options">
+      <div>this is some test content</div>
+    </VicavWinBox>
   </client-only>
 this is a test page
 </template>
