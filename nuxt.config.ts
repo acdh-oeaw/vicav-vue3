9d46c5e5e90ce32165543f2f03b1aa2c48eda88e
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2023-04-03 15:25:56 +0200

    Fix nuxt app setting (application-name)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
     app: {
         head: {
             meta: [{
-                name: 'VICAV Frontend'
+                name: 'application-name',
+                content: 'VICAV Frontend'
             }],
             link: [
                 {
