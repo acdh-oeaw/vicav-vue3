615d9758fcf343c0a57ab95790876272b42ffca7
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2023-04-03 16:47:45 +0200

    logger sample setup

--- store/wm.ts
+++ store/wm.ts
@@ -1,47 +1,54 @@
-import { ref, computed, nextTick } from 'vue';
-import { defineStore } from 'pinia';
+import {ref, computed, nextTick} from 'vue';
+import {defineStore} from 'pinia';
 
 export const useWMStore = defineStore('wm', () => {
 
-	const windowList = ref([] as WindowHandle[])
-	const newWindow = ref(null as NewWindow|null)
+        const windowList = ref([] as WindowHandle[])
+        const newWindow = ref(null as NewWindow | null)
 
-	const Open = (windowTypeId: string, params: Object|null) => {
-		let windowType: WindowType|undefined = knownWindowTypes.find(t => t.id == windowTypeId)
-		if (typeof windowType == 'undefined') {
-			console.error('Invalid window type', windowTypeId)
-			return false;
-		}
-		newWindow.value = { windowType, params }
-		nextTick(() => {
-			newWindow.value = null
-		})
-		console.log('opening', newWindow.value)
-	}
+        const Open = (windowTypeId: string, params: Object | null) => {
+            let windowType: WindowType | undefined = knownWindowTypes.find(t => t.id == windowTypeId)
+            if (typeof windowType == 'undefined') {
+                console.error('Invalid window type', windowTypeId)
+                return false;
+            }
+            newWindow.value = {windowType, params}
+            nextTick(() => {
+                //newWindow.value = null
+            })
+            console.log('opening', newWindow.value)
+        }
 
-	const knownWindowTypes = [
-		{ id: 'map', componentName: 'WMap' },
-		{ id: 'dict-query', componentName: 'DictQuery' },
-	] as WindowType[]
+        const knownWindowTypes = [
+            {id: 'map', componentName: 'WMap'},
+            {id: 'dict-query', componentName: 'DictQuery'},
+        ] as WindowType[]
 
-	interface WindowHandle {
-		id: number
-		ref: null // TODO
-	}
+        interface WindowHandle {
+            id: number
+            ref: null // TODO
+        }
 
-	interface WindowType {
-		id: string
-		componentName: string
-	}
+        interface WindowType {
+            id: string
+            componentName: string
+        }
 
-	interface NewWindow {
-		windowType: WindowType
-		params: Object|null
-	}
+        interface NewWindow {
+            windowType: WindowType
+            params: Object | null
+        }
 
-	return {
-		windowList,
-		newWindow,
-		Open,
-	}
-})
+        return {
+            windowList,
+            newWindow,
+            Open,
+        }
+    },
+
+    {
+        logger: {
+            // only log the decrement action
+            actions: ["Open"],
+        },
+    })
