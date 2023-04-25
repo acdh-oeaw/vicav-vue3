fadff9f413c87d6a3b67cb670e54132bb66feddf
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2023-04-25 11:00:53 +0200

    Remove commented code

--- components/WindowManager.vue
+++ components/WindowManager.vue
@@ -3,13 +3,6 @@
 	import { useWMStore, INewWindow, IWindow, windowTypes } from '~~/store/wm'
 	import { VicavWinBox } from "./VicavWinBox.client"
 
-	/*import { VicavWinBox } from "~/components/VicavWinBox.client";
-	import { LMap, LMarker, LTileLayer, LFeatureGroup } from "@vue-leaflet/vue-leaflet";
-	const options = {
-		x: 500,
-		y: 200
-	}*/
-
     const WMStore = useWMStore()
 	const windowList = computed(() => WMStore.windowList)
 
@@ -56,8 +49,5 @@
 				></component>
 		</VicavWinBox>
 
-		<!--VicavWinBox :options="options">
-            <div>this is some test content</div>
-        </VicavWinBox-->
 	</div>
 </template>
