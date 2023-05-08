ede4297c65f8670c42043b102e66748c6adbb5cb
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2023-05-08 14:59:17 +0200

    Implement submenus in main menu (WIP) #13

--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -2,31 +2,62 @@
     import { useWMStore } from '~~/store/wm';
     const WMStore = useWMStore()
 
-    import { IMenuItem } from '~~/store/appData';
+    import { IMenuItem, IMenuNode } from '~~/store/appData';
     import { useAppDataStore } from '~~/store/appData';
     const AppDataStore = useAppDataStore()
     const menu = computed(() => AppDataStore.appMenu)
 
-    function ClickMenu(menuItem: IMenuItem) {
+/*     function ClickMenu(menuItem: IMenuItem) {
         WMStore.Open(menuItem.windowTypeId, menuItem.params)
     }
-
+                    <!-- @mousedown="ClickMenu(menuItem)" -->
+ */
 </script>
 
 <template>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top">
         <div class="container-fluid">
             <a class="vv-navbar-brand mr-0 mr-md-2" aria-label="Vicav" href="/"><img alt="logo" src="~/assets/vicav_logo.svg"></a>
-            <div class="vv-desktop-menu">
-                <div
-                    v-for="menuItem in menu"
-                    :key="menuItem.name"
-                    @mousedown="ClickMenu(menuItem)"
-                    class="vv-desktop-menu-item"
-                >
-                    {{ menuItem.name }}
-                </div>
-            </div>
+            <ul class="navbar-nav">
+                <li
+                    v-for="menuItem, i in menu"
+                    :key="i"
+                    >
+                    <div
+                        v-if="menuItem.type == 'submenu'"
+                        class="nav-item dropdown"
+                        >
+                        <a
+                            :id="menuItem.id"
+                            class="nav-link dropdown-toggle"
+                            href="#"
+                            role="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                            >
+                            {{ menuItem.name }}
+                        </a>
+                        <div
+                            class="dropdown-menu"
+                            :aria-labelledby="menuItem.id"
+                            >
+                            <div
+                                v-for="m, i in menuItem.submenu"
+                                :key="i"
+                                >
+                                <a
+                                    v-if="m.type == 'item'"
+                                    class="dropdown-item"
+                                    href="#"
+                                    >
+                                    {{ m.name }}
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
             <div>Hamburger menu</div>
             <div>Window selector</div>
         </div>
@@ -42,7 +73,7 @@
         flex-flow: row nowrap;
     }
     .vv-desktop-menu-item {
-        margin: 0 0.5rem;
+        padding: 0 1rem;
         white-space: nowrap;
         cursor: pointer;
     }
--- store/appData.ts
+++ store/appData.ts
@@ -14,19 +14,35 @@ export const useAppDataStore = defineStore(
 				appTitle.value = projectInfo.data.projectConfig.title
 				appMenu.value = projectInfo.data.projectConfig.menu.main.map(m => {
 					return {
+						id: m.id,
+						type: 'submenu',
 						name: m.title,
-						submenu: m.item.map((item: { title: any; componentName: any; id: any; }) => {
-							return {
-								name: item.title,
-								windowTypeId: item.componentName,
-								params: {
-									id: item.id,
-								}
-							} as IMenuItem
+						submenu: m.item.map((item: { id: string; type: string; title: string; componentName: string; }) => {
+							switch (item.type) {
+								case 'item':
+									return {
+										id: item.id,
+										type: 'item',
+										name: item.title,
+										windowTypeId: item.componentName,
+										params: {
+											id: item.id,
+										}
+									} as IMenuItem
+								case 'separator':
+									return {
+										type: 'separator',
+									} as IMenuSeparator
+								case 'dropdown':
+									return {
+										id: item.id,
+										type: 'submenu',
+										name: item.title
+									} as IMenuSubmenu
+							}
 						}),
 					} as IMenuSubmenu
 				})
-				return projectInfo;
 			} catch (error) {
 				console.error(error)
 			}
@@ -41,15 +57,20 @@ export const useAppDataStore = defineStore(
 	}
 )
 
-type IMenuNode = IMenuItem | IMenuSubmenu | IMenuSeparator
+export type IMenuNode = IMenuItem | IMenuSubmenu | IMenuSeparator
 export interface IMenuItem {
+	type: 'item',
+	id: string,
 	name: string,
 	windowTypeId: string,
 	params: null|Object,
 }
 export interface IMenuSubmenu {
+	type: 'submenu',
+	id: string,
 	name: string,
-	submenu: IMenuNode,
+	submenu: IMenuNode[],
 }
 export interface IMenuSeparator {
+	type: 'separator',
 }
