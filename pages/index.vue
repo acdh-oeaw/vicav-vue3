369b58317c1f0d36b6bd8235c56a1d48607cdc13
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2023-05-09 16:51:39 +0200

    some validation and temporary data fixing

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,50 @@
 <script setup lang="ts">
 import { GeoJsonObject } from 'geojson'
+import { z } from "zod";
 const { $api } = useNuxtApp();
+
+const pointSchema = z.object({
+  type: z.literal("Point"),
+  coordinates: z.array(z.number()),
+  bbox: z.array(z.number()).optional()
+})
+
+const featureSchema = z.object({
+  type: z.literal("Feature"),
+  id: z.union([z.number(), z.string()]).optional(),
+  properties: z.any().nullable(),
+  geometry: pointSchema.nullable()
+})
 
 $api.baseUrl = "https://vicav.acdh-ch-dev.oeaw.ac.at/vicav";
 $api.project.getProject({ headers: { accept: "application/json" }});
 const { data, pending, error, refresh } = await useAsyncData(
     () => $api.biblMarkersTei.getMarkers({query: ".*", scope: "geo"}, { headers: { accept: "application/json" }}),
     {
-      transform: (items) => items.data.filter((item) => { return item.properties.name != "al-Jadi" })
+      transform: (items) => items.data.map(feature => {
+        const coordinates = feature.geometry.coordinates.map(coord => parseFloat(coord)).reverse();
+        return {
+          ...feature,
+          geometry: {
+            ...feature.geometry,
+            coordinates
+          }
+        };
+      })
+      .filter((item) => {
+        try {
+          featureSchema.parse(item);
+          return true
+        } catch (error) {
+          console.log(error,item);
+          return false
+        }
+      })
     },
 )
 onMounted(() => {
   refresh()
 })
-console.log(data.value);
 const items = computed(() => data?.value || [])
 </script>
 
