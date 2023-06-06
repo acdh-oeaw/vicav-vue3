3e841835231d235b3e540e64fecf0ef3b626ed9b
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2023-06-06 20:23:42 +0200

    Implement window selector dropdown #13

--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -1,12 +1,38 @@
 <script setup lang="ts">
+    import { Dropdown } from "bootstrap";
     import { useAppDataStore } from '~~/store/appData';
+    import { useWMStore } from '~~/store/wm';
 
     const AppDataStore = useAppDataStore()
     const menu = computed(() => AppDataStore.appMenu)
 
     const isMenuOpen = ref(false)
     function ToggleMenuCollapse(e) {
+        isWindowListOpen.value = false
         isMenuOpen.value = !isMenuOpen.value
+    }
+
+    const WMStore = useWMStore()
+    const windowList = computed(() => {
+        // TODO: populate array
+        return [{ id: null, name: 'No windows open'}]
+    })
+    function SelectWindow(window) {
+        if (window.id != null) {
+            // TODO: focus window
+        }
+    }
+
+    const isWindowListOpen = ref(false)
+    const { $bootstrap } = useNuxtApp()
+    let windowListDropdown: Dropdown;
+    const windowListTogglerRef = ref<HTMLElement | string>('')
+    onMounted(() => {
+        windowListDropdown = new $bootstrap.Dropdown(windowListTogglerRef.value);
+    })
+    function ToggleWindowListCollapse(e) {
+        isMenuOpen.value = false
+        windowListDropdown._isShown() ? windowListDropdown.show() : windowListDropdown.hide();
     }
 </script>
 
@@ -26,15 +52,6 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <button
-                class="navbar-toggler vv-window-selector-toggler"
-                type="button"
-                data-toggle="collapse" data-target="#windowMenu"
-                aria-controls="windowMenu" aria-expanded="false" aria-label="Toggle menu"
-            >
-                <span class="navbar-toggler-icon vv-window-selector-icon" />
-            </button>
-
             <div
                 class="navbar-collapse collapse vv-navbar-menu"
                 :class="{ show: isMenuOpen }"
@@ -48,6 +65,34 @@
                         <VicavMenuNode
                             :menu-node="menuNode"
                         />
+                    </li>
+                </ul>
+            </div>
+
+            <div class="nav-item dropdown">
+                <button
+                    ref="windowListTogglerRef"
+                    class="navbar-toggler vv-window-selector-toggler"
+                    type="button"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-controls="windowList" aria-expanded="false" aria-label="Toggle the window list"
+                    @click="ToggleWindowListCollapse"
+                >
+                    <span class="navbar-toggler-icon vv-window-selector-icon" />
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li
+                        v-for="window in windowList"
+                        :key="window.name"
+                    >
+                        <a
+                            class="dropdown-item"
+                            href="#"
+                            @mousedown="SelectWindow(window)"
+                        >
+                            {{ window.name }}
+                        </a>
                     </li>
                 </ul>
             </div>
@@ -73,5 +118,8 @@
     }
     .vv-window-selector-icon {
         background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3crect stroke='rgba%28255, 255, 255, 0.55%29' stroke-width='2' fill='rgba%280,0,0,0.55%29' width='14' height='18' x='6' y='4'/%3e%3crect stroke='rgba%28255, 255, 255, 0.75%29' stroke-width='2' fill='white' width='14' height='18' x='10' y='9'/%3e%3c/svg%3e")
+    }
+    .vv-navbar-window-list {
+        order: 7;
     }
 </style>
