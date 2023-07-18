fe13cc0c153cb64f9eefa5f87ebbaf42ce0d8734
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2023-07-18 21:38:43 +0200

    Code cleanup [skip ci]

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,31 @@
 <script setup lang="ts">
 	import { useAppDataStore } from '~~/store/appData'
-    const AppDataStore = useAppDataStore()
-    const title = computed(() => AppDataStore.appTitle)
-    watch(title, (title) => { document.title = title })
-import { z } from "zod";
-import {ComputedRef} from "vue";
-const { $api } = useNuxtApp();
+  import { z } from "zod";
+  import {ComputedRef} from "vue";
 
-const pointSchema = z.object({
-  type: z.literal("Point"),
-  coordinates: z.array(z.number()),
-  bbox: z.array(z.number()).optional()
-})
+  const AppDataStore = useAppDataStore()
+  const title = computed(() => AppDataStore.appTitle)
+  watch(title, (title) => { document.title = title })
 
-const featureSchema = z.object({
-  type: z.literal("Feature"),
-  id: z.union([z.number(), z.string()]).optional(),
-  properties: z.any().nullable(),
-  geometry: pointSchema.nullable()
-})
+  const { $api } = useNuxtApp();
 
-type GeoJsonObject = z.infer<typeof featureSchema>
+  const pointSchema = z.object({
+    type: z.literal("Point"),
+    coordinates: z.array(z.number()),
+    bbox: z.array(z.number()).optional()
+  })
 
-$api.baseUrl = ("" + import.meta.env.VITE_APIBASEURL);
-const { data, pending, error, refresh } = await useAsyncData(
+  const featureSchema = z.object({
+    type: z.literal("Feature"),
+    id: z.union([z.number(), z.string()]).optional(),
+    properties: z.any().nullable(),
+    geometry: pointSchema.nullable()
+  })
+
+  type GeoJsonObject = z.infer<typeof featureSchema>
+
+  $api.baseUrl = ("" + import.meta.env.VITE_APIBASEURL);
+  const { data, pending, error, refresh } = await useAsyncData(
     () => $api.biblMarkersTei.getMarkers({query: ".*", scope: "geo"}, { headers: { accept: "application/json" }}),
     {
       transform: (items) => items.data.filter((item) => {
@@ -36,17 +38,17 @@ const { data, pending, error, refresh } = await useAsyncData(
         }
       })
     },
-)
-onMounted(() => {
-  refresh()
-})
-const items: ComputedRef<GeoJsonObject[]> = computed(() => data?.value as GeoJsonObject[] || [])
+  )
+  onMounted(() => {
+    refresh()
+  })
+  const items: ComputedRef<GeoJsonObject[]> = computed(() => data?.value as GeoJsonObject[] || [])
 </script>
 
 <template>
   <MenuBar/>
-    <client-only>
-      <WindowManager/>
-      <vicav-map :items="items" style="height:calc(100vh - 68px); position: relative; top: -36px; outline: none;"></vicav-map>
-    </client-only>
+  <client-only>
+    <WindowManager/>
+    <vicav-map :items="items" style="height:calc(100vh - 68px); position: relative; top: -36px; outline: none;"></vicav-map>
+  </client-only>
 </template>
