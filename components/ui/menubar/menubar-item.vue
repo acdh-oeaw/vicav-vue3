ca3d3df0528ab78c5f2180a552f235d15d47de87
Stefan Probst <stefanprobst@posteo.de>
2023-09-28 08:14:25 +0200

    style: fix menu item focus style

--- components/ui/menubar/menubar-item.vue
+++ components/ui/menubar/menubar-item.vue
@@ -12,7 +12,7 @@ const emitAsProps = useEmitAsProps(emit);
 	<MenubarItem
 		v-bind="{ ...props, ...emitAsProps }"
 		:class="[
-			'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm focus-visible:bg-accent focus-visible:text-on-accent focus-visible:outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+			'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm focus:bg-accent focus:text-on-accent focus:outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
 			{ 'pl-8': inset },
 		]"
 	>
