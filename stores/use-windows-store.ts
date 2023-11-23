2ed1960fbac69ff123c9acd66379ef10a8c1dad2
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2023-11-23 14:06:21 +0100

    Fiy initial screen window list encoding #4

--- stores/use-windows-store.ts
+++ stores/use-windows-store.ts
@@ -117,7 +117,7 @@ export const useWindowsStore = defineStore("windows", () => {
 	async function initializeScreen() {
 		await navigateTo({
 			path: "/",
-			query: { w: "[]", a: arrangement.value },
+			query: { w: atob("[]"), a: arrangement.value },
 		});
 	}
 
