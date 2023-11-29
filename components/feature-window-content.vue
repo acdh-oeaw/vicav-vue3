4b2e2e0337206d781b6c32c60e5e8d76523af210
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2023-11-29 16:06:46 +0100

    Add css stylesheet for features #81

--- components/feature-window-content.vue
+++ components/feature-window-content.vue
@@ -29,8 +29,80 @@ const isLoading = computed(() => {
 </template>
 
 <style>
-/* stylelint-disable-next-line selector-class-pattern */
-.tbHeader {
-	margin: 0;
+/* stylelint-disable selector-class-pattern, color-no-hex, color-named, color-function-notation */
+
+.tbFeatures {
+	width: 90%;
+	border: 1px solid rgb(89, 83, 60);
+}
+
+.tdFeaturesCom {
+	background: #e7e2ca;
+}
+
+.tdFeaturesHead {
+	border: 1px solid rgb(168, 93, 143);
+	background: rgb(168, 93, 143);
+	color: white;
+	font-size: large;
+}
+
+.tdFeaturesHeadRight {
+	vertical-align: top;
+	padding-right: 5px;
+	border: 1px solid rgb(168, 93, 143);
+	background: rgb(168, 93, 143);
+	color: white;
+	font-size: large;
+	text-align: right;
+}
+
+.tdFeaturesLeft {
+	vertical-align: top;
+	width: 110px;
+	padding-left: 3px;
+	border: 1px solid rgb(168, 93, 143);
+	background: rgb(222, 222, 222);
+	color: rgb(168, 93, 143);
+}
+
+.iFeaturesTrans {
+	color: blue;
+	font-size: small;
+}
+
+.tdFeaturesRightSource {
+	vertical-align: top;
+	width: 80%;
+	padding-left: 3px;
+	border: 1px solid rgb(168, 93, 143);
+	color: #7f960a;
+}
+
+.tdFeaturesRightTarget {
+	vertical-align: top;
+	width: 80%;
+	padding-left: 3px;
+	border: 1px solid rgb(168, 93, 143);
+	background: rgb(246, 236, 225);
+	color: #a58103;
+	font-style: italic;
+}
+
+/* Compare samples window */
+
+a.word-search {
+	color: inherit;
+}
+
+.w {
+	color: inherit;
+	font-style: normal;
+}
+
+.w.highlight {
+	padding: 0;
+	background: 0;
+	color: red;
 }
 </style>
