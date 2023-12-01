8c26352b9918306ed9c900300eb1fc5c901d36ff
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2023-12-01 15:09:34 +0100

    Transform old stylesheet to apply tailwind #81

--- components/feature-window-content.vue
+++ components/feature-window-content.vue
@@ -29,80 +29,49 @@ const isLoading = computed(() => {
 </template>
 
 <style>
-/* stylelint-disable selector-class-pattern, color-no-hex, color-named, color-function-notation */
+/* stylelint-disable selector-class-pattern */
 
 .tbFeatures {
-	width: 90%;
-	border: 1px solid rgb(89, 83, 60);
+	@apply w-11/12 border border-solid border-[#59533c];
 }
 
 .tdFeaturesCom {
-	background: #e7e2ca;
+	@apply bg-[#e7e2ca];
 }
 
 .tdFeaturesHead {
-	border: 1px solid rgb(168, 93, 143);
-	background: rgb(168, 93, 143);
-	color: white;
-	font-size: large;
+	@apply border border-solid border-primary bg-primary text-on-primary text-lg;
 }
 
 .tdFeaturesHeadRight {
-	vertical-align: top;
-	padding-right: 5px;
-	border: 1px solid rgb(168, 93, 143);
-	background: rgb(168, 93, 143);
-	color: white;
-	font-size: large;
-	text-align: right;
+	@apply align-top pr-[5px] border border-solid border-primary text-on-primary text-lg text-right;
 }
 
 .tdFeaturesLeft {
-	vertical-align: top;
-	width: 110px;
-	padding-left: 3px;
-	border: 1px solid rgb(168, 93, 143);
-	background: rgb(222, 222, 222);
-	color: rgb(168, 93, 143);
+	@apply align-top w-[110px] pl-[3px] border border-solid border-primary bg-on-primary text-primary;
 }
 
 .iFeaturesTrans {
-	color: blue;
-	font-size: small;
+	@apply text-blue-600 text-xs;
 }
 
 .tdFeaturesRightSource {
-	vertical-align: top;
-	width: 80%;
-	padding-left: 3px;
-	border: 1px solid rgb(168, 93, 143);
-	color: #7f960a;
+	@apply align-top w-4/5 pl-[3px] border border-solid border-primary bg-[#a85d8f] text-[#7f960a];
 }
 
 .tdFeaturesRightTarget {
-	vertical-align: top;
-	width: 80%;
-	padding-left: 3px;
-	border: 1px solid rgb(168, 93, 143);
-	background: rgb(246, 236, 225);
-	color: #a58103;
-	font-style: italic;
+	@apply align-top w-4/5 pl-[3px] border border-solid border-primary bg-on-primary text-[#a58103] italic;
 }
 
-/* Compare samples window */
-
 a.word-search {
-	color: inherit;
+	@apply text-inherit;
 }
 
 .w {
-	color: inherit;
-	font-style: normal;
+	@apply text-inherit not-italic;
 }
 
 .w.highlight {
-	padding: 0;
-	background: 0;
-	color: red;
+	@apply p-0 bg-inherit text-red-600;
 }
 </style>
