054c3b3fbe1fbdab4513cd3bfac813cf5ce519b4
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2023-12-01 17:25:58 +0100

    refact: create props, remove inner stylesheet #33

--- components/input-extended.vue
+++ components/input-extended.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-defineProps(["modelValue"]);
+defineProps({
+	modelValue: String,
+	stringSnippets: Array<string>,
+});
 const emit = defineEmits(["update:modelValue"]);
 
 const inputElement = ref();
@@ -9,42 +12,13 @@ watch(myString, (val) => {
 	emit("update:modelValue", val);
 });
 
-const specChars = [
-	"&#x2019;",
-	"&#x0294;",
-	"&#x0101;",
-	"&#x1e05;",
-	"&#x0295;",
-	"&#x1e0f;",
-	"&#x1e0f;&#x0323;",
-	"&#x0113;",
-	"&#x0121;",
-	"&#x01e7;",
-	"&#x1e25;",
-	"&#x012b;",
-	"&#x1d35;",
-	"&#x1e37;",
-	"&#x1e43;",
-	"&#x014d;",
-	"&#x1e5b;",
-	"&#x1e63;",
-	"&#x0073;&#x0320;",
-	"&#x0161;",
-	"&#x1e6d;",
-	"&#x1e6f;",
-	"&#x016b;",
-	"&#x1e93;",
-	"&#x017e;",
-];
-
 let cursorPosition = 0;
-const InsertChar = async (htmlChar: string): Promise<void> => {
-	let decodedChar = parseHtmlEntities(htmlChar);
+const InsertSnippet = async (snippet: string): Promise<void> => {
 	myString.value =
 		myString.value.substring(0, cursorPosition) +
-		decodedChar +
+		snippet +
 		myString.value.substring(cursorPosition, myString.value.length);
-	cursorPosition += decodedChar.length;
+	cursorPosition += snippet.length;
 	await nextTick();
 	setCursorPosition(cursorPosition);
 };
@@ -64,38 +38,31 @@ const setCursorPosition = (pos: number) => {
 	}
 };
 
-function parseHtmlEntities(str: string) {
-	return str.replace(/&#x([0-9a-f]{1,4});/gi, function (match, numStr) {
-		var num = parseInt(numStr, 16);
-		return String.fromCharCode(num);
-	});
-}
-
-const registerCursorPosition = (e: Event) => {
+const recordCursorPosition = (e: Event) => {
 	cursorPosition = Number((e.target as HTMLInputElement).selectionStart);
 };
 </script>
 
 <template>
-	<div>
-		<!-- eslint-disable-next-line vue/no-v-html -->
-		<button
-			v-for="(c, i) in specChars"
-			:key="i"
-			class="bg-gray-300 px-4 py-2 font-bold text-gray-800 hover:bg-gray-400"
-			@click="InsertChar(c)"
-			v-html="c"
-		></button>
-	</div>
-	<div>
-		<!-- eslint-disable-next-line vuejs-accessibility/form-control-has-label -->
-		<input
-			ref="inputElement"
-			v-model="myString"
-			type="text"
-			class="m-2 rounded border px-3 py-2 leading-tight shadow"
-			@keyup="registerCursorPosition"
-			@click="registerCursorPosition"
-		/>
+	<!-- eslint-disable tailwindcss/no-custom-classname -->
+	<div class="ei">
+		<div class="ei-buttons">
+			<button
+				v-for="(c, i) in stringSnippets"
+				:key="i"
+				@click="InsertSnippet(c)"
+				v-text="c"
+			></button>
+		</div>
+		<div class="ei-textinput">
+			<!-- eslint-disable-next-line vuejs-accessibility/form-control-has-label -->
+			<input
+				ref="inputElement"
+				v-model="myString"
+				type="text"
+				@keyup="recordCursorPosition"
+				@click="recordCursorPosition"
+			/>
+		</div>
 	</div>
 </template>
--- components/spec-char-input-demo-window-content.vue
+++ components/spec-char-input-demo-window-content.vue
@@ -1,14 +1,48 @@
-<script setup>
+<script setup lang="ts">
 const myString = ref("");
+
+const specialCharacters: Array<string> = [
+	"ē",
+	"ṛ",
+	"ṯ",
+	"ẓ",
+	"ū",
+	"ī",
+	"ō",
+	"ā",
+	"š",
+	"ḏ",
+	"ṭ",
+	"ǧ",
+	"ḥ",
+	"ž",
+	"ṣ",
+	"ḏ̣",
+	"ʕ",
+	"ʔ",
+	"ġ",
+	"ḅ",
+	"ṃ",
+];
 </script>
 
 <template>
 	<div>
 		<div>
-			<InputExtended v-model="myString" />
+			<InputExtended v-model="myString" :string-snippets="specialCharacters" />
 		</div>
 		<div>
 			{{ myString }}
 		</div>
 	</div>
 </template>
+
+<style>
+.ei button {
+	@apply bg-gray-300 px-4 py-2 font-bold text-gray-800 hover:bg-gray-400;
+}
+
+.ei input {
+	@apply m-2 border px-3 py-2 shadow;
+}
+</style>
