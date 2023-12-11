a37f886046d79fb2198e78407a705b96c2fc315f
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2023-12-11 19:26:51 +0100

    Create form for bibliographic query #89

--- components/biblio-entries-window-content.vue
+++ components/biblio-entries-window-content.vue
@@ -7,10 +7,25 @@ const props = defineProps<Props>();
 const { params } = toRefs(props);
 
 const { data, isPending, isPlaceholderData } = useBiblioTeiQuery(params);
+const openNewWindowFromAnchor = useAnchorClickHandler();
 
 const isLoading = computed(() => {
 	return isPending.value || isPlaceholderData.value;
 });
+
+const queryString: Ref<string> = ref(params.value.query);
+const isTextQuery: Ref<boolean> = ref(true);
+const isMapQuery: Ref<boolean> = ref(false);
+
+function submitNewQuery(): void {
+	if (!isTextQuery.value && !isMapQuery.value) isTextQuery.value = true;
+	if (isTextQuery.value) {
+		params.value.query = queryString.value;
+	}
+	if (isMapQuery.value) {
+		console.log("map query not implemented");
+	}
+}
 </script>
 
 <template>
@@ -18,6 +33,78 @@ const isLoading = computed(() => {
 		class="relative isolate grid h-full w-full overflow-auto"
 		:class="{ 'opacity-50 grayscale': isLoading }"
 	>
+		{{ queryString }} {{ isTextQuery }} {{ isMapQuery }}
+		<!-- eslint-disable vuejs-accessibility/form-control-has-label, tailwindcss/no-custom-classname -->
+		<div class="max-w-3xl px-8 pb-4 pt-8">
+			<div class="dvStats">
+				Query:&nbsp;&nbsp;
+				<span class="spQueryText">{{ queryString }}</span>
+			</div>
+			<div class="max-w-3xl bg-gray-200 px-4">
+				<div class="pt-3">
+					<input
+						v-model="queryString"
+						type="text"
+						class="block w-full rounded border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+						placeholder="Search in bibliographies&hellip;"
+					/>
+				</div>
+				<div class="my-3">
+					<button class="biblQueryBtn" @click="submitNewQuery">Query</button>
+				</div>
+				<div class="mb-5 mt-2 flex">
+					<div class="flex flex-1 items-start">
+						<div class="flex h-5 items-center">
+							<input
+								id="isTextQuery"
+								v-model="isTextQuery"
+								type="checkbox"
+								class="focus:ring-3 h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800"
+							/>
+						</div>
+						<label
+							for="isTextQuery"
+							class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+						>
+							Display as text
+						</label>
+					</div>
+					<div class="flex flex-1 items-start">
+						<div class="flex h-5 items-center">
+							<input
+								id="isMapQuery"
+								v-model="isMapQuery"
+								type="checkbox"
+								class="focus:ring-3 h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-primary dark:focus:ring-offset-gray-800"
+							/>
+						</div>
+						<label
+							for="isMapQuery"
+							class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+						>
+							Display on map
+						</label>
+					</div>
+				</div>
+				<div class="pb-4">
+					<p>
+						For details as to how to formulate meaningful queries in the bibliography
+						<a
+							class="aVicText"
+							href="/"
+							data-target-type="Text"
+							data-text-id="li_vicavExplanationBibliography"
+							@click="openNewWindowFromAnchor"
+						>
+							click here
+						</a>
+						<!-- TODO: remove the linebreak before the punctuation so that it sticks to the html end tag and find the linting rule to ignore -->
+						.
+					</p>
+				</div>
+			</div>
+		</div>
+
 		<!-- eslint-disable-next-line vue/no-v-html, vuejs-accessibility/click-events-have-key-events, vuejs-accessibility/no-static-element-interactions -->
 		<div v-if="data" class="prose max-w-3xl p-8" v-html="data" />
 
@@ -59,5 +146,13 @@ const isLoading = computed(() => {
 .dvBibBook .fa-file-text,
 .dvBibBookSection .fa-file-text {
 	@apply block;
+}
+
+.biblQueryBtn {
+	@apply w-full h-10 text-primary border-2 border-solid border-primary font-bold bg-on-primary inline-block text-center whitespace-nowrap align-middle rounded;
+}
+
+.biblQueryBtn:hover {
+	@apply bg-primary text-on-primary;
 }
 </style>
