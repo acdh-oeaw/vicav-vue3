1c85e8527d31a29b5edd9bb210ae68ba20317057
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2024-01-23 11:50:24 +0100

    Add teiSource to zod validation #97

--- stores/use-windows-store.ts
+++ stores/use-windows-store.ts
@@ -5,6 +5,7 @@ import { z } from "zod";
 import {
 	QueryString,
 	Schema,
+	TeiSource,
 	TextId,
 	type WindowItem,
 	type WindowItemTargetType,
@@ -158,7 +159,8 @@ export const useWindowsStore = defineStore("windows", () => {
 			root: rootElement,
 		});
 
-		if (ci.success) {
+		const teiSourceParse = TeiSource.safeParse(params);
+		if (teiSourceParse.success) {
 			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 			winbox.addControl({
 				index: 0,
@@ -166,7 +168,7 @@ export const useWindowsStore = defineStore("windows", () => {
 				click: function () {
 					const env = useRuntimeConfig();
 					if (env.public.NUXT_PUBLIC_TEI_BASEURL) {
-						window.open(`${env.public.NUXT_PUBLIC_TEI_BASEURL}&id=${ci.data.textId}`, "_blank");
+						window.open(teiSourceParse.data.teiSource, "_blank");
 					}
 				},
 			});
