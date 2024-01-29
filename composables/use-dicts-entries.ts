c65835e68d736e6969f91868355d69cbc543ce4d
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2024-01-29 16:06:09 +0100

    feat: basic error handling in dict query entries #73

--- composables/use-dicts-entries.ts
+++ composables/use-dicts-entries.ts
@@ -14,10 +14,35 @@ export function useDictsEntries(
 		enabled: options?.enabled,
 		queryKey: ["get-dicts-entries", params] as const,
 		async queryFn({ queryKey: [, params] }) {
-			const response = await api.restvle.getDictDictNameEntries(params.dictId, params.queryParams, {
-				headers: { accept: "application/json" },
-			});
-			return response.data;
+			if (!params.queryParams?.q) return null;
+			try {
+				const response = await api.restvle.getDictDictNameEntries(
+					params.dictId,
+					params.queryParams,
+					{
+						headers: { accept: "application/json" },
+					},
+				);
+				return response.data;
+			} catch (e) {
+				if (e.status === null) {
+					console.error(e);
+					return null;
+				}
+				switch (e.status) {
+					case 404: {
+						return {
+							total_items: "0",
+						};
+						break;
+					}
+					default: {
+						break;
+					}
+				}
+				console.error(e);
+				return null;
+			}
 		},
 	});
 }
