5d132307f1dfa430a0a87b504ba3f45450d1232e
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-02-09 14:31:03 +0100

    accordion components

---
+++ components/ui/accordion/Accordion.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {
+	AccordionRoot,
+	type AccordionRootEmits,
+	type AccordionRootProps,
+	useEmitAsProps,
+} from "radix-vue";
+
+const props = defineProps<AccordionRootProps>();
+const emits = defineEmits<AccordionRootEmits>();
+
+const emitAsProps = useEmitAsProps(emits);
+</script>
+
+<template>
+	<AccordionRoot v-bind="{ ...props, ...emitAsProps }">
+		<slot />
+	</AccordionRoot>
+</template>
---
+++ components/ui/accordion/AccordionContent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { AccordionContent, type AccordionContentProps } from "radix-vue";
+
+const props = withDefaults(defineProps<AccordionContentProps>(), {});
+</script>
+
+<template>
+	<AccordionContent
+		v-bind="props"
+		class="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm transition-all"
+	>
+		<div class="pb-4 pt-0" :class="$attrs.class">
+			<slot />
+		</div>
+	</AccordionContent>
+</template>
