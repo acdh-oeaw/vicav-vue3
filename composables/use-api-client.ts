1d7484e293a354f1d14fa36c85b499d936193ffc
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-02-27 16:43:01 +0100

    fix: change apiBaseUrl env var name

--- composables/use-api-client.ts
+++ composables/use-api-client.ts
@@ -5,8 +5,8 @@ const api = new Api();
 export function useApiClient() {
 	const env = useRuntimeConfig();
 
-	if (env.public.NUXT_PUBLIC_API_BASE_URL) {
-		api.baseUrl = env.public.NUXT_PUBLIC_API_BASE_URL;
+	if (env.public.apiBaseUrl) {
+		api.baseUrl = env.public.apiBaseUrl;
 	}
 
 	return api;
