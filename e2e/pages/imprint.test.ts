d0268f4d5191a684367d8395fcda687ceebb444f
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-03-13 10:24:36 +0100

    fix: tests in workflow

--- e2e/pages/imprint.test.ts
+++ e2e/pages/imprint.test.ts
@@ -3,7 +3,18 @@ import { expect, test } from "@playwright/test";
 test.describe("imprint page", () => {
 	test("should have document title", async ({ page }) => {
 		await page.goto("/imprint");
-		await expect(page).toHaveTitle("Imprint | VICAV3.0 - Vienna Corpus of Arabic Varieties");
+		if (
+			process.env.K8S_SECRET_NUXT_PUBLIC_API_BASE_URL === "https://vicav.acdh-ch-dev.oeaw.ac.at/"
+		) {
+			await expect(page).toHaveTitle("Imprint | VICAV3.0 - Vienna Corpus of Arabic Varieties");
+		} else if (
+			process.env.K8S_SECRET_NUXT_PUBLIC_API_BASE_URL ===
+			"https://wibarab-api.acdh-ch-dev.oeaw.ac.at/"
+		) {
+			await expect(page).toHaveTitle("Imprint | WIBARAB");
+		} else {
+			await expect(page).toHaveTitle("Imprint | VICAV3.0 - Vienna Corpus of Arabic Varieties");
+		}
 	});
 
 	test("should have imprint text", async ({ page }) => {
--- e2e/pages/index.test.ts
+++ e2e/pages/index.test.ts
@@ -3,6 +3,17 @@ import { expect, test } from "@playwright/test";
 test.describe("home page", () => {
 	test("should have document title", async ({ page }) => {
 		await page.goto("/");
-		await expect(page).toHaveTitle("Home | VICAV3.0 - Vienna Corpus of Arabic Varieties");
+		if (
+			process.env.K8S_SECRET_NUXT_PUBLIC_API_BASE_URL === "https://vicav.acdh-ch-dev.oeaw.ac.at/"
+		) {
+			await expect(page).toHaveTitle("Home | VICAV3.0 - Vienna Corpus of Arabic Varieties");
+		} else if (
+			process.env.K8S_SECRET_NUXT_PUBLIC_API_BASE_URL ===
+			"https://wibarab-api.acdh-ch-dev.oeaw.ac.at/"
+		) {
+			await expect(page).toHaveTitle("Home | WIBARAB");
+		} else {
+			await expect(page).toHaveTitle("Home | VICAV3.0 - Vienna Corpus of Arabic Varieties");
+		}
 	});
 });
