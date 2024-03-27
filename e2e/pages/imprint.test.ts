3572f1ec3f5d24185187ab08b793bc7b7b3e5aed
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-03-27 13:30:42 +0100

    test: tests improved

--- e2e/pages/imprint.test.ts
+++ e2e/pages/imprint.test.ts
@@ -1,17 +1,20 @@
 import { expect, test } from "@playwright/test";
 
 test.describe("imprint page", () => {
-	console.log(process.env.NUXT_PUBLIC_API_BASE_URL);
 	test("should have document title", async ({ page }) => {
 		await page.goto("/imprint");
-		if (process.env.NUXT_PUBLIC_API_BASE_URL === "https://vicav.acdh-ch-dev.oeaw.ac.at") {
-			await expect(page).toHaveTitle("Imprint | VICAV3.0 - Vienna Corpus of Arabic Varieties");
-		} else if (
-			process.env.NUXT_PUBLIC_API_BASE_URL === "https://wibarab-api.acdh-ch-dev.oeaw.ac.at"
-		) {
-			await expect(page).toHaveTitle("Imprint | WIBARAB");
-		} else {
-			await expect(page).toHaveTitle("Imprint | TUNOCENT");
+		switch (process.env.NUXT_PUBLIC_API_BASE_URL) {
+			case "https://vicav.acdh-ch-dev.oeaw.ac.at":
+				await expect(page).toHaveTitle("Imprint | VICAV3.0 - Vienna Corpus of Arabic Varieties");
+				break;
+			case "https://wibarab-api.acdh-ch-dev.oeaw.ac.at":
+				await expect(page).toHaveTitle("Imprint | WIBARAB");
+				break;
+			case "https://tunocent-vicav.acdh-dev.oeaw.ac.at":
+				await expect(page).toHaveTitle("Imprint | TUNOCENT");
+				break;
+			default:
+				await expect(page).toHaveTitle("Imprint | VICAV3.0 - Vienna Corpus of Arabic Varieties");
 		}
 	});
 
--- e2e/pages/index.test.ts
+++ e2e/pages/index.test.ts
@@ -3,14 +3,18 @@ import { expect, test } from "@playwright/test";
 test.describe("home page", () => {
 	test("should have document title", async ({ page }) => {
 		await page.goto("/");
-		if (process.env.NUXT_PUBLIC_API_BASE_URL === "https://vicav.acdh-ch-dev.oeaw.ac.at") {
-			await expect(page).toHaveTitle("Home | VICAV3.0 - Vienna Corpus of Arabic Varieties");
-		} else if (
-			process.env.NUXT_PUBLIC_API_BASE_URL === "https://wibarab-api.acdh-ch-dev.oeaw.ac.at"
-		) {
-			await expect(page).toHaveTitle("Home | WIBARAB");
-		} else {
-			await expect(page).toHaveTitle("Home | TUNOCENT");
+		switch (process.env.NUXT_PUBLIC_API_BASE_URL) {
+			case "https://vicav.acdh-ch-dev.oeaw.ac.at":
+				await expect(page).toHaveTitle("Home | VICAV3.0 - Vienna Corpus of Arabic Varieties");
+				break;
+			case "https://wibarab-api.acdh-ch-dev.oeaw.ac.at":
+				await expect(page).toHaveTitle("Home | WIBARAB");
+				break;
+			case "https://tunocent-vicav.acdh-dev.oeaw.ac.at":
+				await expect(page).toHaveTitle("Home | TUNOCENT");
+				break;
+			default:
+				await expect(page).toHaveTitle("Imprint | VICAV3.0 - Vienna Corpus of Arabic Varieties");
 		}
 	});
 });
