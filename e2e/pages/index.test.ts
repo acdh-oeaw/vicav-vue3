d696d0560e457209a11f6ec7530b6ed4668d3323
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-03-27 13:18:24 +0100

    fix: test adjustment

--- e2e/pages/index.test.ts
+++ e2e/pages/index.test.ts
@@ -3,10 +3,10 @@ import { expect, test } from "@playwright/test";
 test.describe("home page", () => {
 	test("should have document title", async ({ page }) => {
 		await page.goto("/");
-		if (process.env.NUXT_PUBLIC_API_BASE_URL === "https://vicav.acdh-ch-dev.oeaw.ac.at/") {
+		if (process.env.NUXT_PUBLIC_API_BASE_URL === "https://vicav.acdh-ch-dev.oeaw.ac.at") {
 			await expect(page).toHaveTitle("Home | VICAV3.0 - Vienna Corpus of Arabic Varieties");
 		} else if (
-			process.env.NUXT_PUBLIC_API_BASE_URL === "https://wibarab-api.acdh-ch-dev.oeaw.ac.at/"
+			process.env.NUXT_PUBLIC_API_BASE_URL === "https://wibarab-api.acdh-ch-dev.oeaw.ac.at"
 		) {
 			await expect(page).toHaveTitle("Home | WIBARAB");
 		} else {
