164466e8d8c4959948201b70cd761ca830ca0b13
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-03-27 11:44:25 +0100

    fix: test adjustment

--- e2e/pages/imprint.test.ts
+++ e2e/pages/imprint.test.ts
@@ -4,12 +4,12 @@ test.describe("imprint page", () => {
 	test("should have document title", async ({ page }) => {
 		await page.goto("/imprint");
 		if (
-			process.env.K8S_SECRET_NUXT_PUBLIC_API_BASE_URL === "https://vicav.acdh-ch-dev.oeaw.ac.at/"
+			process.env.K8S_SECRET_NUXT_PUBLIC_API_BASE_URL === "https://vicav.acdh-ch-dev.oeaw.ac.at"
 		) {
 			await expect(page).toHaveTitle("Imprint | VICAV3.0 - Vienna Corpus of Arabic Varieties");
 		} else if (
 			process.env.K8S_SECRET_NUXT_PUBLIC_API_BASE_URL ===
-			"https://wibarab-api.acdh-ch-dev.oeaw.ac.at/"
+			"https://wibarab-api.acdh-ch-dev.oeaw.ac.at"
 		) {
 			await expect(page).toHaveTitle("Imprint | WIBARAB");
 		} else {
