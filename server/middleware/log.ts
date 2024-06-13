902904f1aedd782510432c091ee9e569001686c9
Omar Siam <Omar.Siam@oeaw.ac.at>
2024-06-13 19:00:45 +0200

    Add the recommended pino-http-print module and use it

--- server/middleware/log.ts
+++ server/middleware/log.ts
@@ -1,6 +1,17 @@
 import pino from "pino-http";
 
-const logger = pino();
+const logger = pino({
+	quietReqLogger: true,
+	transport: {
+		target: "pino-http-print",
+		options: {
+			destination: 1,
+			all: true,
+			colorize: false,
+			translateTime: true,
+		},
+	},
+});
 
 export default defineEventHandler((event) => {
 	logger(event.node.req, event.node.res);
