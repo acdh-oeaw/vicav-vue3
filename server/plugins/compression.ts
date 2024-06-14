33396d5b2d26a70ba552f312479e5be26954bf4c
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-06-14 16:03:59 +0200

    bug: identified compression as culprit for the WTTFB lag

--- server/plugins/compression.ts
+++ server/plugins/compression.ts
@@ -2,6 +2,8 @@ import { useCompression } from "h3-compression";
 
 export default defineNitroPlugin((nitro) => {
 	nitro.hooks.hook("render:response", async (response, { event }) => {
+		console.time("Compression");
 		await useCompression(event, response);
+		console.timeEnd("Compression");
 	});
 });
