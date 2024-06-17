d6733846020f55c7998044bed1ea543bd53b775c
Omar Siam <simar@gmx.net>
2024-06-17 19:41:55 +0200

    Log as a plugin instead of as middleware

--- server/middleware/log.ts
+++
@@ -1,8 +0,0 @@
-import { ecsFormat } from "@elastic/ecs-pino-format";
-import { type LoggerOptions, pino } from "pino";
-
-const logger = pino(ecsFormat({ convertReqRes: true }) as LoggerOptions);
-
-export default defineEventHandler((event) => {
-	logger.info({ req: event.node.req, res: event.node.res }, `Handled request`);
-});
