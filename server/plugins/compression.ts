6901dafa84bf9eb4adf75f45cc992a2e1468bc12
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-06-18 12:46:50 +0200

    fix: a little type enhancement
    # Conflicts:
    #	pnpm-lock.yaml

--- server/plugins/compression.ts
+++ server/plugins/compression.ts
@@ -1,16 +1,21 @@
-/* eslint-disable */
 import { getRequestHeader, type H3Event, setResponseHeader } from "h3";
+import type { RenderResponse } from "nitropack";
 
 // This is the main function of h3-compression. As it is the only thing we really use and the
 // rest of the code contains surprises (br silently ignored) it is copied here.
-function compressStream(event: H3Event, response: Partial<RenderResponse>, method: string) {
+function compressStream(
+	event: H3Event,
+	response: Partial<RenderResponse>,
+	method: CompressionFormat,
+) {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 	const stream = new Response(response.body).body;
-	setResponseHeader(event, "Content-Encoding", method);
-	response.body = stream.pipeThrough(new CompressionStream(method));
+	setResponseHeader(event, "Content-Encoding", method as unknown as string);
+	response.body = stream?.pipeThrough(new CompressionStream(method));
 }
 
 export default defineNitroPlugin((nitro) => {
-	nitro.hooks.hook("render:response", async (response, { event }) => {
+	nitro.hooks.hook("render:response", (response, { event }) => {
 		const encoding = getRequestHeader(event, "accept-encoding");
 		// CompressionStream does not support brotli yet!
 		// if (encoding?.includes("br")) {
