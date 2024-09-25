ecb20b29ba5bc08e1b1cda2bfd22a20331f1c09a
Omar Siam <Omar.Siam@oeaw.ac.at>
2024-09-25 18:19:37 +0200

    Use the extracetd functions in util

--- components/data-list-window-content.vue
+++ components/data-list-window-content.vue
@@ -15,117 +15,19 @@ const props = defineProps<Props>();
 
 const { simpleItems /* rawItems */ } = useTEIHeaders();
 // Four grouping levels: country, region, place, dataType
-type groupedByDataType = Record<DataTypesEnum, Array<simpleTEIMetadata>>;
-type groupedByAnyString = Record<string, Array<simpleTEIMetadata>>;
-type groupedByPlace = Record<string, groupedByAnyString>;
-type groupedByRegion = Record<string, groupedByPlace>;
-type groupedItemType = Record<string, groupedByRegion>;
-type GroupedTypes = groupedByAnyString | groupedByPlace | groupedByRegion | groupedItemType;
+type groupedByDataType = Record<DataTypesEnum, Array<simpleTEIMetadata>>; //L4
+type groupedByPlace = Record<string, groupedByDataType>; //L3
+type groupedByRegion = Record<string, groupedByPlace>; //L2
+type groupedByCountry = Record<string, groupedByRegion>; //L1
 
-// https://gist.github.com/andrewchilds/30a7fb18981d413260c7a36428ed13da?permalink_comment_id=4433741#gistcomment-4433741
-type GetReturnType<T> = T | undefined;
-type ValueType = Record<string | number, unknown>;
-
-function deepGet<T>(
-	value: unknown,
-	query: string | Array<string | number>,
-	defaultVal: GetReturnType<T> = undefined,
-): GetReturnType<T> {
-	const splitQuery = Array.isArray(query)
-		? query
-		: query
-				.replace(/(\[(\d)\])/g, ".$2")
-				.replace(/^\./, "")
-				.split(".");
-
-	if (!splitQuery.length || splitQuery[0] === undefined) return value as T;
-
-	const key = splitQuery[0];
-
-	if (
-		typeof value !== "object" ||
-		value === null ||
-		!(key in value) ||
-		(value as ValueType)[key] === undefined
-	) {
-		return defaultVal;
-	}
-
-	return deepGet((value as ValueType)[key], splitQuery.slice(1), defaultVal);
-}
-
-const orderByGroup = function (unordered: Record<string, Array<simpleTEIMetadata>>) {
-	return Object.keys(unordered)
-		.sort()
-		.reduce((obj: Record<string, Array<simpleTEIMetadata>>, key: string) => {
-			obj[key] = unordered[key]!;
-			return obj;
-		}, {});
-};
-
-const getGroupedItems = function (dataTypesFilter: Array<string>) {
-	// Group by country
-	const collectedItems = simpleItems.value
-		.filter((item) => {
-			return (
-				dataTypesFilter.includes(deepGet(item, "dataType") ?? "") &&
-				((deepGet(item, "dataType") ?? "") !== "CorpusText" || (deepGet(item, "hasTEIw") ?? false))
-			);
-		})
-		.sort((a, b) => {
-			return (deepGet<string>(a, "label") ?? "").localeCompare(deepGet(b, "label") ?? "");
-		});
-
-	const groupedByCountry = Object.groupBy(collectedItems, (item: simpleTEIMetadata) => {
-		return deepGet(item, "place.country") ?? "";
-	}) as GroupedTypes;
-
-	// Group by region
-	for (const country in groupedByCountry as Record<string, Array<simpleTEIMetadata>>) {
-		(groupedByCountry as groupedByPlace)[country] = orderByGroup(
-			Object.groupBy(
-				(groupedByCountry as Record<string, Array<simpleTEIMetadata>>)[country]!,
-				(item: simpleTEIMetadata) => {
-					return deepGet(item, "place.region") ?? "";
-				},
-			),
-		);
-
-		// Group by place
-		for (const region in (groupedByCountry as groupedByPlace)[country]) {
-			(groupedByCountry as groupedByRegion)[country]![region] = orderByGroup(
-				Object.groupBy(
-					(groupedByCountry as groupedByPlace)[country]![region]!,
-					(item: simpleTEIMetadata) => {
-						return deepGet(item, "place.settlement") ?? "";
-					},
-				),
-			);
-
-			// Group by content type
-
-			for (const place in (groupedByCountry as groupedByRegion)[country]![region]) {
-				(groupedByCountry as groupedItemType)[country]![region]![place] = Object.groupBy(
-					(groupedByCountry as groupedByRegion)[country]![region]![place]!,
-					(item) => {
-						return deepGet(item, "dataType") ?? "";
-					},
-				);
-
-				for (const dataType in (groupedByCountry as groupedItemType)[country]![region]![place]!) {
-					(groupedByCountry as groupedItemType)[country]![region]![place]![dataType] = (
-						groupedByCountry as groupedItemType
-					)[country]![region]![place]![dataType]!.sort((a, b) => {
-						return (deepGet<string>(a, "label") ?? "").localeCompare(deepGet(b, "label") ?? "");
-					});
-				}
-			}
-		}
-	}
-
-	return groupedByCountry as groupedItemType;
-};
-const groupedItems = getGroupedItems(props.params.dataTypes);
+const groupedItems = getGroupedItems(
+	simpleItems.value,
+	["place.country", "place.region", "place.settlement", "dataType"],
+	"dataType",
+	props.params.dataTypes,
+	"label",
+	props.params.filterListBy,
+) as groupedByCountry;
 const openNewWindowFromAnchor = useAnchorClickHandler();
 
 const debugString = debug ? JSON.stringify(groupedItems, null, 2) : "";
