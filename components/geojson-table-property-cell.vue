8203c379c7e4a45ec631e65e076217de10f50e96
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-10-18 16:26:13 +0200

    fix: cell if value missing

--- components/geojson-table-property-cell.vue
+++ components/geojson-table-property-cell.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
 const props = defineProps<{
-	value: Record<string, unknown>;
+	value: Record<string, unknown> | undefined;
 }>();
 </script>
 
 <template>
-	<Badge v-for="(key, value) in props.value" :key="value" class="mr-1" variant="outline">
-		<div class="flex flex-col">
-			<p class="line-clamp-1">{{ value }}</p>
-			<Badge v-for="(subkey, subvalue) in key" :key="subvalue" class="ml-1" variant="secondary">
-				<p class="line-clamp-1">{{ subvalue }}</p>
-				<Badge v-for="(subsubkey, index) in subkey" :key="index" class="ml-1" variant="secondary">
-					<Badge class="ml-1" variant="default">{{ subsubkey }}</Badge>
+	<div v-if="value">
+		<Badge v-for="(key, value) in props.value" :key="value" class="mr-1" variant="outline">
+			<div class="flex flex-col">
+				<p class="line-clamp-1">{{ value }}</p>
+				<Badge v-for="(subkey, subvalue) in key" :key="subvalue" class="ml-1" variant="secondary">
+					<p class="line-clamp-1">{{ subvalue }}</p>
+					<Badge v-for="(subsubkey, index) in subkey" :key="index" class="ml-1" variant="secondary">
+						<Badge class="ml-1" variant="default">{{ subsubkey }}</Badge>
+					</Badge>
 				</Badge>
-			</Badge>
-		</div>
-	</Badge>
+			</div>
+		</Badge>
+	</div>
+	<span v-else>N/A</span>
 </template>
