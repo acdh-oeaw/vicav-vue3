fe6b0517fa34abea2eb36f1bc798e10ea7227658
Omar Siam <Omar.Siam@oeaw.ac.at>
2024-10-24 15:17:01 +0200

    Add label to the parameters from element text
    Not only create a window title.

--- composables/use-anchor-click-handler.ts
+++ composables/use-anchor-click-handler.ts
@@ -10,20 +10,22 @@ export function useAnchorClickHandler() {
 	function openNewWindowFromAnchor(event: MouseEvent) {
 		const element = event.target as HTMLElement;
 
-		let item: Record<string, string> | null = null;
+		let item: Record<string, string> = {};
 		if (element instanceof HTMLAnchorElement) {
 			item = element.dataset as Record<string, string>;
 		} else if (element.parentElement instanceof HTMLAnchorElement) {
 			item = element.parentElement.dataset as Record<string, string>;
 		}
 
-		if (item?.targetType) {
+		const params = jsonStringsToObject(item);
+		params.label = isNonEmptyString(params.label) ? params.label : element.innerText;
+		if (item.targetType) {
 			if (item.targetType === "External-link") return;
 			event.preventDefault();
 			addWindow({
-				targetType: item.targetType,
-				params: jsonStringsToObject(item),
-				title: isNonEmptyString(item.label) ? item.label : element.innerText,
+				targetType: params.targetType,
+				params: params,
+				title: params.label,
 			} as WindowState);
 		}
 	}
