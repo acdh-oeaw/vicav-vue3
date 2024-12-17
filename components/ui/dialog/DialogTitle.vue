82ea445a6fea19654acb836506f74a7bbae4a286
Katharina Wünsche <katharina_wunsche@yahoo.de>
2024-12-17 11:14:44 +0100

    chore: add dialog component

---
+++ components/ui/dialog/DialogTitle.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { DialogTitle, type DialogTitleProps, useForwardProps } from "radix-vue";
+import { twMerge } from "tailwind-merge";
+import { computed, type HTMLAttributes } from "vue";
+
+const props = defineProps<DialogTitleProps & { class?: HTMLAttributes["class"] }>();
+
+const delegatedProps = computed(() => {
+	const { class: _, ...delegated } = props;
+
+	return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+	<DialogTitle
+		v-bind="forwardedProps"
+		:class="twMerge('text-lg font-semibold leading-none tracking-tight', props.class)"
+	>
+		<slot />
+	</DialogTitle>
+</template>
