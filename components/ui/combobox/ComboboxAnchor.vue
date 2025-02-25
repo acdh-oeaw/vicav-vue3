1fd69c46dfc76f01626fe6f222d0e727d707b6ba
Katharina Wünsche <katharina_wunsche@yahoo.de>
2025-02-25 12:50:49 +0100

    chore: add shadcn combobox

---
+++ components/ui/combobox/ComboboxAnchor.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ComboboxAnchor, type ComboboxAnchorProps, useForwardProps } from "reka-ui";
+import { twMerge } from "tailwind-merge";
+import { computed, type HTMLAttributes } from "vue";
+
+const props = defineProps<ComboboxAnchorProps & { class?: HTMLAttributes["class"] }>();
+
+const delegatedProps = computed(() => {
+	const { class: _, ...delegated } = props;
+
+	return delegated;
+});
+
+const forwarded = useForwardProps(delegatedProps);
+</script>
+
+<template>
+	<ComboboxAnchor v-bind="forwarded" :class="twMerge('w-[200px]', props.class)">
+		<slot />
+	</ComboboxAnchor>
+</template>
---
+++ components/ui/combobox/ComboboxItem.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {
+	ComboboxItem,
+	type ComboboxItemEmits,
+	type ComboboxItemProps,
+	useForwardPropsEmits,
+} from "reka-ui";
+import { twMerge } from "tailwind-merge";
+import { computed, type HTMLAttributes } from "vue";
+
+const props = defineProps<ComboboxItemProps & { class?: HTMLAttributes["class"] }>();
+const emits = defineEmits<ComboboxItemEmits>();
+
+const delegatedProps = computed(() => {
+	const { class: _, ...delegated } = props;
+
+	return delegated;
+});
+
+const forwarded = useForwardPropsEmits(delegatedProps, emits);
+</script>
+
+<template>
+	<ComboboxItem
+		v-bind="forwarded"
+		:class="
+			twMerge(
+				'relative flex cursor-default gap-2 select-none justify-between items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:size-4 [&_svg]:shrink-0',
+				props.class,
+			)
+		"
+	>
+		<slot />
+	</ComboboxItem>
+</template>
