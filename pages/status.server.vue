cf30c1865689d36c9574b49a38694ee287185466
Omar Siam <Omar.Siam@oeaw.ac.at>
2025-10-14 16:22:28 +0200

    Add a server site rendered status page with minimal html framework

---
+++ pages/status.server.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+definePageMeta({
+	title: "Status",
+	layout: "plain",
+});
+
+const { data } = useProjectInfo();
+</script>
+
+<template>
+	<pre>
+Backend response:
+ETag: {{ data?.ETag ?? "" }}
+baseURIPublic: {{ data?.projectConfig?.baseURIPublic ?? "" }}
+took: {{ data?.took ?? "" }}
+	</pre
+	>
+</template>
+
+<style>
+#__nuxt {
+	overflow: scroll;
+	block-size: 100%;
+}
+</style>
