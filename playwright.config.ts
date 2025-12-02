b2d6ff60bea3d7380cfe7aff8d7f06c5a2153969
Omar Siam <Omar.Siam@oeaw.ac.at>
2025-12-02 18:40:42 +0100

    Add a switch for the different backends here
    This selects tests in a named subdirectory.

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,35 @@ import { isCI } from "ci-info";
 const port = 3000;
 const baseUrl = `http://localhost:${port.toString()}`;
 
+const backend = (() => {
+	// eslint-disable-next-line @typescript-eslint/switch-exhaustiveness-check
+	switch (process.env.NUXT_PUBLIC_API_BASE_URL) {
+		case "https://vicav-dev.acdh.oeaw.ac.at":
+		case "https://vicav.acdh.oeaw.ac.at":
+			// http://localhost:8984 needs to be moved according to the backend running
+			return "vicav";
+		case "https://shawi.acdh-ch-dev.oeaw.ac.at":
+		case "http://localhost:8984":
+			// case "https://shawi-api.acdh.oeaw.ac.at": This is the next production backend.
+			return "shawi";
+		case "https://tunocent-22a417b1-9f07-406b-9098-288b3f981d99.acdh-ch-dev.oeaw.ac.at/":
+		case "https://tunocent-api.acdh.oeaw.ac.at/":
+			return "tunocent";
+		case "https://wibarab-api.acdh-ch-dev.oeaw.ac.at/":
+			return "wibarab";
+		default:
+			// in VSCode there is "playwright.env" in settings.conf for the playwright running in the UI.
+			// Example:
+			//   "playwright.env": {
+			//     "NUXT_PUBLIC_API_BASE_URL": "http://localhost:8984"
+			//   }
+			// }
+			// If nothing is set in settings.conf file or the OS environment the UI will show the tests
+			// failing here:
+			throw new Error("unknown backend");
+	}
+})();
+
 export default defineConfig({
 	testDir: "./e2e",
 	fullyParallel: true,
@@ -13,6 +42,7 @@ export default defineConfig({
 	retries: isCI ? 2 : 0,
 	workers: isCI ? 1 : undefined,
 	reporter: "html",
+	testMatch: `${backend}/*.test.ts`,
 	use: {
 		baseURL: baseUrl,
 		trace: "on-first-retry",
