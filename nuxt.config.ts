46e8fd249e9790936726e0bc21457d22e3b9c802
Katharina Wünsche <katharina_wunsche@yahoo.de>
2025-12-16 11:15:03 +0100

    chore: update tile layer url in env example and nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,7 +83,7 @@ export default defineNuxtConfig({
 			apiBaseUrl: "https://vicav-dev.acdh.oeaw.ac.at",
 			appBaseUrl: "http://localhost:3000",
 			mapTileLayerAttribution:
-				"https://api.mapbox.com/styles/v1/acdh-ch-tech/cllp2n7aj004a01r7fl637bq1/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWNkaC1jaC10ZWNoIiwiYSI6ImNsZXpnMDdpODBlc2Q0MGwzOGZmaWNveDgifQ.yYx8e9PdsDl-NzOpGXAL7g",
+				"https://api.mapbox.com/styles/v1/acdh/cmj1dhylp005x01r4dw4p9ghx/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWNkaCIsImEiOiJjbWoxOHBsNjEwZjc5M2VyNzBvZ2x2ejlkIn0.itr6_jU4L58IgS2H8aDySA",
 			mapTileLayerUrl:
 				'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
 			matomoBaseUrl: "",
