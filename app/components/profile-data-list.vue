2edab66e8248652ff68a1a05042cc54368cfbabc
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2026-07-03 17:50:23 +0200

    feat: use filter list for profiles as well

---
+++ app/components/profile-data-list.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import SimpleMetadataDataList from "@/components/simple-metadata-data-list.vue";
+import type { simpleTEIMetadata } from "@/types/teiCorpus.ts";
+
+defineProps<{
+	items: Array<simpleTEIMetadata>;
+}>();
+</script>
+
+<template>
+	<SimpleMetadataDataList
+		data-type="Profile"
+		:items="items"
+		:require-tei-availability-for-link="false"
+		search-input-id="profile-search"
+		:show-audio-availability="false"
+		target-type="Profile"
+	/>
+</template>
