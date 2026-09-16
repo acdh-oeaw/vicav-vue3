9b4c42c479a545ab41c50f95e194f9b5e7b3bc39
Katharina Wünsche <katharina_wunsche@yahoo.de>
2026-09-16 16:32:11 +0200

    fix: format

--- app/components/corpus-query-window-content.vue
+++ app/components/corpus-query-window-content.vue
@@ -178,7 +178,9 @@ function splitUtterancesAroundHit(utterances: MixedUtteranceContent, hitId?: str
 	};
 }
 
-const { cqlConfig: attributeConfig } = useCqlAttributes({corpname: config.value?.projectConfig?.noskeHost ?? ""});
+const { cqlConfig: attributeConfig } = useCqlAttributes({
+	corpname: config.value?.projectConfig?.noskeHost ?? "",
+});
 
 const cqlConfig = computed<CqlConfig>(() =>
 	attributeConfig.value.map((attr) =>
