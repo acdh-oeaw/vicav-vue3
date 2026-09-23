262f5beff46af5e01d4a5e8696d73bbf95d04c1d
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2026-09-23 08:31:25 +0200

    fix: adjusting tests

--- e2e/pages/vicav/map.test.ts
+++ e2e/pages/vicav/map.test.ts
@@ -20,6 +20,6 @@ test.describe("map page", () => {
 		await page.getByRole("navigation").getByRole("button", { name: "Bibliographies" }).click();
 		await page.getByLabel("Bibliographies").getByText("All Bibl. Locations on Map").click();
 		await expect(page.locator(".leaflet-control-container")).toBeVisible();
-		await expect(page.locator("img:nth-child(218)")).toBeVisible();
+		await expect(page.locator(".leaflet-marker-icon").first()).toBeVisible({ timeout: 15_000 });
 	});
 });
